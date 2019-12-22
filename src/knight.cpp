afe04bcf20148870e095f54393f33db4f09be21a
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-23 01:02:21 +0330

    init(knight): empty function definition

---
+++ src/knight.cpp
@@ -0,0 +1,15 @@
+#include "collapse/core.h"
+#include "collapse/core/pieces.h"
+
+namespace collapse {
+namespace core {
+
+bool
+Knight::isValidMove(const IPoint &src, const IPoint &dst) override
+{
+	//
+}
+
+}  // namespace core
+}  // namespace collapse
+
