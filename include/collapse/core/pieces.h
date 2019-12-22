ea7e3a7e61e2d9194e3ee6bfbdfeb35f5c2524fc
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-22 21:09:38 +0330

    init(core): declare piece class variants in pieces.h

--- include/collapse/core/pieces.h
+++ include/collapse/core/pieces.h
@@ -1,4 +1,48 @@
 #ifndef COLLAPSE_CORE_PIECES_H_
 #define COLLAPSE_CORE_PIECES_HPP
 
+#include "collapse/core.h"
+
+namespace collapse {
+namespace core {
+
+class King : public Piece {
+ public:
+    King(Type type) : type(type), role(PieceRole.KING) {};
+    bool isValidMove(const IPoint &src, const IPoint &dst) override;
+};
+
+class Queen : public Piece {
+ public:
+    Queen(Type type) : type(type), role(PieceRole.QUEEN) {};
+    bool isValidMove(const IPoint &src, const IPoint &dst) override;
+};
+
+class Bishop : public Piece {
+ public:
+    Bishop(Type type) : type(type), role(PieceRole.BISHOP) {};
+    bool isValidMove(const IPoint &src, const IPoint &dst) override;
+};
+
+class Knight : public Piece {
+ public:
+    Knight(Type type) : type(type), role(PieceRole.KNIGHT) {};
+    bool isValidMove(const IPoint &src, const IPoint &dst) override;
+};
+
+class Rook : public Piece {
+ public:
+    Rook(Type type) : type(type), role(PieceRole.ROOK) {};
+    bool isValidMove(const IPoint &src, const IPoint &dst) override;
+};
+
+class Pawn : public Piece {
+ public:
+    Pawn(Type type) : type(type), role(PieceRole.PAWN) {};
+    bool isValidMove(const IPoint &src, const IPoint &dst) override;
+};
+
+}  // namespace core
+}  // namespace collapse
+
 #endif  // COLLAPSE_CORE_PIECES_H_
