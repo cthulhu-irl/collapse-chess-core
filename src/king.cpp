db9d98b4a8f3ba14235b51152e2e7b2f2988f193
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-23 01:01:42 +0330

    init(king): empty function definition

---
+++ src/king.cpp
@@ -0,0 +1,15 @@
+#include "collapse/core.h"
+#include "collapse/core/pieces.h"
+
+namespace collapse {
+namespace core {
+
+bool
+King::isValidMove(const IPoint &src, const IPoint &dst) override
+{
+	//
+}
+
+}  // namespace core
+}  // namespace collapse
+
