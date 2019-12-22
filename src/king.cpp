44216b407865b7cf9816d11c7ef40f5a9216a631
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-23 02:28:07 +0330

    develop: implement King.isValidMove

--- src/king.cpp
+++ src/king.cpp
@@ -5,11 +5,34 @@ namespace collapse {
 namespace core {
 
 bool
-King::isValidMove(const IPoint &src, const IPoint &dst) override
+King::isValidMove(const IMap &map, const IPoint &src, const IPoint &dst) override
 {
-	//
+    // is it a castling move?
+    if (src.getX() == 5 && (src.getY() % 8) <= 1)  // check if king is in initial state
+    {
+        // check if selected destination is next to an initial rook
+        char delta_x = dst.getX() - src.getX();
+        if (abs(delta_x) == 2 && (dst.getY() % 8) <= 1)
+        {
+            // check if there's an original rook near dest
+            if (map[dst.getY()][delta_x < 0 ? 1 : 8]->getRole() != PieceRole.ROOK)
+                return false;
+
+            // check if there aren't anything between
+            char stepper = (abs(delta_x) + 1) - delta_x; // or delta_x & 0x70
+            if (map[dst.getY()][dst.getX() - stepper  ] == nullptr) return false;
+            if (map[dst.getY()][dst.getX() - stepper*2] == nullptr) return false;
+
+            return true;
+        }
+    }
+
+    // OR is it only one block away?
+    if ((dst.getX() - src.getX()) == 1 || (dst.getY() - src.getY()) == 1)
+        return true;
+
+    return false;
 }
 
 }  // namespace core
 }  // namespace collapse
-
