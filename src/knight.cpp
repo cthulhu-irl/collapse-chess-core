85f826ad132e1ce0a936c3277cc6c5f7cb03940b
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-23 04:37:39 +0330

    develop: implement Knight.isValidMove in knight.cpp

--- src/knight.cpp
+++ src/knight.cpp
@@ -7,7 +7,11 @@ namespace core {
 bool
 Knight::isValidMove(const IMap &map, const IPoint &src, const IPoint &dst) override
 {
-	//
+    // check the L case, either x or y must be 1 or 2, but not the same
+    if ((dst.getX() - src.getX() + dst.getY() - src.getY()) == 3)
+        return true;
+
+    return false;
 }
 
 }  // namespace core
