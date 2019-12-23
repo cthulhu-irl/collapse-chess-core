56ab1034207077b5077bce5148d04e6be5ba5dd9
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-23 10:05:17 +0330

    fix(knight): avoid collision in checks

--- src/knight.cpp
+++ src/knight.cpp
@@ -11,7 +11,10 @@ Knight::isValidMove(const IMap &map, const IPoint &src, const IPoint &dst) overr
     char delta_y = dst.getY() - src.getY();
 
     // check the L case, either x or y must be 1 or 2, but not the same
-    if (abs(delta_x & delta_y) == 3)
+    if (abs(delta_x) == 1 && abs(delta_y) == 2)
+        return true;
+
+    if (abs(delta_x) == 2 && abs(delta_y) == 1)
         return true;
 
     return false;
