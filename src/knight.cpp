ad63af52d8d9fc6347cdae90a639a9a9336a264a
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-23 05:15:22 +0330

    fix(knight): use bitwise and to prevent collision

--- src/knight.cpp
+++ src/knight.cpp
@@ -7,8 +7,11 @@ namespace core {
 bool
 Knight::isValidMove(const IMap &map, const IPoint &src, const IPoint &dst) override
 {
+    char delta_x = dst.getX() - src.getX();
+    char delta_y = dst.getY() - src.getY();
+
     // check the L case, either x or y must be 1 or 2, but not the same
-    if ((dst.getX() - src.getX() + dst.getY() - src.getY()) == 3)
+    if (abs(delta_x & delta_y) == 3)
         return true;
 
     return false;
