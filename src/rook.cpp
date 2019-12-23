6bf5b8ba617651ec4410474fa6d0208aa81af9be
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-23 05:36:16 +0330

    develop: implement Rook.isValidMove in rook.cpp

--- src/rook.cpp
+++ src/rook.cpp
@@ -7,9 +7,25 @@ namespace core {
 bool
 Rook::isValidMove(const IMap &map, const IPoint &src, const IPoint &dst) override
 {
-	//
+	char delta_x = dst.getX() - src.getX();
+    char delta_y = dst.getY() - src.getY();
+
+    // check if either vertical linear or horizontal
+    if (delta_x != 0 && delta_y != 0)
+        return false;
+
+    // check if nothing between
+    char x_step = delta_x & 0x71, y_step = delta_y & 0x71;
+    char x_end = dst.getX(), y_end = dst.getY();
+
+    for (char x = src.getX() + x_step - 1, y = src.getY() + y_step - 1;
+            x != x_end || y != y_end;
+            (x += x_step) & (y += y_step))
+    {
+        if (map[y][x] != nullptr)
+            return false;
+    }
 }
 
 }  // namespace core
 }  // namespace collapse
-
