6dec6ebb9ae6c857e6be4deb8fda86e31667d85c
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-27 13:02:59 +0330

    develop: implement Point's constructor in point.cpp

--- src/point.cpp
+++ src/point.cpp
@@ -3,24 +3,40 @@
 using namespace collapse;
 using namespace core;
 
+/// Take `x` and `y` as coordinates, and save them as 0..7 numbers
+/// `x` can be 1..8 or a..h as in chess board
+/// `y` can only be 1..8
 Point::Point(unsigned char x, unsigned char y)
 {
-    // check if 1 <= x <= 8, then assign
-    // otherwise if 'a' <= x <= 'h', then convert and assign
+    // 1..8 range
+    if (1 <= x && x <= 8)
+        this->x = x-1;
+
+    // a..h range
+    else if ('a' <= x && x <= 'h')
+        this->x = x-'a';
+
     // otherwise throw an exception
+    else
+        throw "PointException should be replaced in here";
 
     // check if 1 <= y <= 8, then assign
+    if (1 <= y && y <= 8)
+        this->y = y-1;
+
     // otherwise throw an exception
+    else
+        throw "PointException should be replaced in here";
 }
 
 unsigned char
 Point::getX() override
 {
-    return x;
+    return this->x;
 }
 
 unsigned char
 Point::getY() override
 {
-    return y;
+    return this->y;
 }
