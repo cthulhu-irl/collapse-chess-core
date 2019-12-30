a08f9f48e5b9f5c6f04bf540b840efc7081a4671
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-30 22:13:15 +0330

    fix: abs checks and no -1 offsets as point is 0-7 now

--- src/bishop.cpp
+++ src/bishop.cpp
@@ -13,12 +13,15 @@ Bishop::isValidMove(const IMap<IPiece> &map,
     char delta_y = dst.getY() - src.getY();
 
     // check if diagonal linear
-    if (delta_x != delta_y)
+    if (abs(delta_x) != abs(delta_y))
         return false;
 
     // check if nothing between
-    char x_step = (delta_x|1) & 0x81, y_step = (delta_y|1) & 0x81;
-    char x_end = dst.getX(), y_end = dst.getY();
+    char x_step = (delta_x < 0) ? -1 : 1;
+    char y_step = (delta_y < 0) ? -1 : 1;
+
+    char x_end = dst.getX();
+    char y_end = dst.getY();
 
     for (char x = src.getX() + x_step, y = src.getY() + y_step;
             x != x_end || y != y_end;
