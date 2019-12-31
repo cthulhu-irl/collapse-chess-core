eff1a720aa2b09907bc2e4d88584702c7e7773d6
xh4x0r3r <xh4x0r3r@gmail.com>
2019-12-31 12:39:51 +0330

    develop: implement King with fixed assumptions

--- src/king.cpp
+++ src/king.cpp
@@ -9,32 +9,38 @@ King::isValidMove(const IMap<IPiece> &map,
                   const IPoint &src,
                   const IPoint &dst) const
 {
-    /*
+    char delta_x = dst.getX() - src.getX();
+    char delta_y = dst.getY() - src.getY();
+
     // is it a castling move?
     // check if king is in its initial point
-    if (src.getX() == 5 && (src.getY() % 8) <= 1)
+    if (src.getX() == 4 && (src.getY() % 7) == 0)
     {
         // check if selected destination is next to an initial rook
-        char delta_x = dst.getX() - src.getX();
-        if (abs(delta_x) == 2 && (dst.getY() % 8) <= 1)
+        if (abs(delta_x) == 2 && (dst.getY() % 7) == 0)
         {
             // check if there's an original rook near dest
-            if (map[dst.getY()][delta_x < 0 ? 1 : 8]->getRole() != PieceRole.ROOK)
+            // another way:
+            //      ((delta_x|1 & 0x81) + 6) & 7 => 0 || 7
+            if (map((delta_x < 0) ? 0 : 7, dst.getY())
+                    ->getRole() != PieceRole::ROOK)
                 return false;
 
             // check if there aren't anything between
-            char stepper = (abs(delta_x) + 1) - delta_x; // or delta_x & 0x81
-            if (map[dst.getY()][dst.getX() - stepper  ] == nullptr) return false;
-            if (map[dst.getY()][dst.getX() - stepper*2] == nullptr) return false;
+            char stepper = (abs(delta_x) + 1) - delta_x;
+            if (map(dst.getX() - stepper, dst.getY()) == nullptr)
+                return false;
+
+            if (map(dst.getX() - stepper*2, dst.getY()) == nullptr)
+                return false;
 
             return true;
         }
     }
 
     // OR is it only one block away?
-    if ((dst.getX() - src.getX()) == 1 || (dst.getY() - src.getY()) == 1)
+    if (abs(delta_x) == 1 || abs(delta_y) == 1)
         return true;
-    */
 
     return false;
 }
