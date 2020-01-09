a25ed3724bb908b225d5459e0a8997edfb31c472
xh4x0r3r <xh4x0r3r@gmail.com>
2020-01-09 23:14:04 +0330

    feat: add StackList declarations

---
+++ src/stack_list.cpp
@@ -0,0 +1,35 @@
+#include "stack_list.h"
+
+template<typename T>
+StackList<T>::StackList()
+{
+    //
+}
+
+template<typename T>
+T &
+StackList<T>::push(T item)
+{
+    //
+}
+
+template<typename T>
+T
+StackList<T>::pop()
+{
+    //
+}
+
+template<typename T>
+T &
+StackList<T>::next()
+{
+    //
+}
+
+template<typename T>
+T &
+StackList<T>::prev()
+{
+    //
+}
