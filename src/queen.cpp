3b1efd68c0da14cda80bbde741b3116e19f4203c
xh4x0r3r <xh4x0r3r@gmail.com>
2020-01-25 23:22:06 +0330

    develop: implement Queen::countWalkPointList

--- src/queen.cpp
+++ src/queen.cpp
@@ -71,5 +71,40 @@ Queen::genWalkPointList(const IMap<IPiece> &map,
     return list;
 }
 
+size_t
+Queen::countWalkPointList(const IMap<IPiece> &map,
+                            const IPoint &src) const
+{
+    size_t count = 0;
+    int x, y;
+    int x_step, y_step;
+
+    // x directions
+    for (x_step = -1; x_step <= 1; x_step++)
+    {
+        // y directions
+        for (y_step = -1; y_step <= 1; y_step++)
+        {
+            // each step in x_step,y_step direction
+            for (x=src.getX()+x_step, y=src.getY()+y_step;
+                    (0 <= x && x <= 7) && (0 <= y && y <= 7);
+                    x += x_step, y += y_step)
+            {
+                // get the piece at x,y as dest if there is any
+                const IPiece *piece = map(x, y);
+
+                // add the point to list if no piece or opposite type
+                if (!piece || piece->getType() != getType())
+                    count++;
+                
+                // if a piece was found, there's no point to continue
+                if (piece) break;
+            }
+        }
+    }
+
+    return count;
+}
+
 }  // namespace core
 }  // namespace collapse
